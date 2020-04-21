37ef53d8be449133ae45cfaba81aee6c248f85ae
Chris <chris.wittyname@gmail.com>
2020-04-21 20:27:41 +1200

    Fixes for build

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
-import AppButton from './components/AppButton/AppButton';
+import AButton from './components/AButton/AButton';
+import AAddIcon from './components/icons/AAddIcon';
+
 import defaultTheme from './themes/default';
+
+const icons = {
+  AAddIcon,
+};
 
 const themes = {
   default: defaultTheme,
 };
 
 export {
-  AppButton,
+  AButton,
+  icons,
   themes,
 };
